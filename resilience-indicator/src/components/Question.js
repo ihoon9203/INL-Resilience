9b6a0b5a64e04fd92809d5fe9511b28564a0df82
ANTONIO ARCEO <u1271272@umail.utah.edu>
2021-12-12 22:42:06 -0700

    Added hide/reveal functionality of subquestions when clicking yes/no on survey questions

--- resilience-indicator/src/components/Question.js
+++ resilience-indicator/src/components/Question.js
@@ -1,33 +1,53 @@
 import React from "react";
 import QuestionList from "./QuestionList";
 import RadioButtonSet from "./RadioButtonSet";
-import { makeStyles } from "@material-ui/core/styles";
-import { Card, CardContent, Chip, Grid, Typography } from '@material-ui/core';
+import { Card, CardContent, Grid, Typography } from '@material-ui/core';
 import useStyles from '../styles';
 
-const sublistStyle = {
-    color: 'blue',
-    textIndent: '40px'
+// Couldn't use the useStyles() method as before so I had to use this in file styling.
+const cardStyle = {
+    card: {
+        margin: '30px 20px 20px 20px',
+    }
 }
 
-const Question = ({ question }) => (
+class Question extends React.Component {
 
-    <Card className={useStyles().card}>
-        <CardContent>
-            <Grid container spacing={1} justify="left">
-                <Grid item xs={.5}>{question.id}</Grid>
-                <Grid item xs={10}>
-                    <Typography>{question.text}</Typography>
-                    <RadioButtonSet/>
-                </Grid>
-            </Grid>
-            <li>
-                {question.sub_questions &&
-                    <QuestionList style={sublistStyle} questions={question.sub_questions} />
-                }
-            </li>
-        </CardContent>
-    </Card>
-);
+    constructor(props) {
+        super(props); // super() must be called before using 'this.'
+        this.question = this.props.question
+        this.state = {
+            answerVal: '',
+        };
+        this.myChangeHandler = this.myChangeHandler.bind(this);
+    }
+    myChangeHandler(event) {
+        this.setState({
+            ["answerVal"]: event.target.value
+        });
+
+    }
+    render() {
+        return (
+            <Card style={cardStyle.card}>
+                <CardContent>
+                    <Grid container spacing={1} justify="left">
+                        <Grid item xs={.5}>{this.question.id}</Grid>
+                        <Grid item xs={10}>
+                            <Typography>{this.question.text}</Typography>
+                            <RadioButtonSet myChangeHandler={this.myChangeHandler}
+                                answerVal={this.state.answerVal} />
+                        </Grid>
+                    </Grid>
+                    <li>
+                        {this.question.subquestions && this.state.answerVal == "Yes" &&
+                            <QuestionList questions={this.question.subquestions} />
+                        }
+                    </li>
+                </CardContent>
+            </Card>
+        )
+    }
+}
 
 export default Question;
--- resilience-indicator/src/components/RadioButtonSet.js
+++ resilience-indicator/src/components/RadioButtonSet.js
@@ -1,29 +1,25 @@
-import React from "react";
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 
-
-import Container from '@mui/material/Container';
-import Stack from '@mui/material/Stack';
-import { purple } from "@mui/material/colors";
-
-
-const RadioButtonSet = () => (
-
-  <div className="App">
-    <FormControl component="fieldset">
-      <RadioGroup row aria-label="gender" name="row-radio-buttons-group">
-        <FormControlLabel value="yes" control={<Radio size="small"  />} label="Yes" />
-        <FormControlLabel value="no" control={<Radio size="small"  />} label="No" />
-        <FormControlLabel value="n/a" control={<Radio size="small"  />} label="N/A" />
-      </RadioGroup>
-    </FormControl>
-  </div>
-
-
-);
+class RadioButtonSet extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <FormControl component="fieldset">
+          <RadioGroup row aria-label="question" name="row-radio-buttons-group" value={this.props.answerVal}
+            onChange={this.props.myChangeHandler}>
+            <FormControlLabel value="Yes" name="answer" control={<Radio size="small" />} label="Yes" />
+            <FormControlLabel value="No" name="answer" control={<Radio size="small" />} label="No" />
+            <FormControlLabel value="N/A" name="answer" control={<Radio size="small" />} label="N/A" />
+          </RadioGroup>
+        </FormControl>
+      </div>
+    )
+  }
+}
 
 export default RadioButtonSet;
